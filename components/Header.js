016debddca19e180a4ae2ed419bb5fabab2aa6ef
Anant Gaire <56.anant@gmail.com>
2021-06-02 20:38:50 +0545

    header and auth done

--- components/Header.js
+++ components/Header.js
@@ -14,8 +14,11 @@ import {
   ShoppingCartIcon,
 } from '@heroicons/react/outline';
 import HeaderIcon from './HeaderIcon';
+import { useSession } from 'next-auth/client';
+import { signOut } from 'next-auth/client';
 
 const Header = () => {
+  const [session] = useSession();
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-2 shadow-md">
       {/* Header left */}
@@ -47,9 +50,18 @@ const Header = () => {
       </div>
       {/* header right */}
       <div className="flex items-center sm:space-x-2 justify-end">
-        {/* <Image  /> */}
+        <Image
+          onClick={signOut}
+          className="rounded-full cursor-pointer"
+          src={session.user.image}
+          width="40"
+          height="40"
+          layout="fixed"
+        />
 
-        <p className="whitespace-nowrap font-semibold pr-3">Anant Gaire</p>
+        <p className="whitespace-nowrap font-semibold pr-3">
+          {session.user.name}
+        </p>
         <ViewGridIcon className="icon" />
         <ChatIcon className="icon" />
         <BellIcon className="icon" />
